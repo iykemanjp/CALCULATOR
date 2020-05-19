7b232c1105cd5bc5df363da740b3aff46546e586
iykemanjp <61423913+iykemanjp@users.noreply.github.com>
2020-05-19 12:34:32 +0100

    Add files via upload
    MY FIRST EVER JAVASCRIPT

---
+++ script.js
@@ -0,0 +1,31 @@
+ // document.getElementById("container").onchange = () => {
+ //   let bill = Number(document.getElementById("billTotal").value);
+ //   let tipPercent = document.getElementById("tipInput").value;
+ //    let split = document.getElementById("splitInput").value;
+ //    let tipValue = bill * (tipPercent / 100);
+ //   let newBillEach = (bill + tipValue) / split;
+ //   let tipEach = tipValue / split;
+  
+ //    // output
+ //    document.getElementById("tipOutput").innerHTML = tipPercent + "%";
+ //    document.getElementById("splitOutput").innerHTML = split;
+ //    document.getElementById("newBill").innerHTML = "$" + newBillEach.toFixed(2);
+ //     document.getElementById("tipEach").innerHTML = "$" + tipEach.toFixed(2);
+
+
+
+document.getElementById("container").onchange = () => {
+    let bill = Number(document.getElementById("billTotal").value);
+    let tipPercent = document.getElementById("tipInput").value;
+    let split = document.getElementById("splitInput").value;
+    let tipValue = bill * ( tipPercent / 100);
+    let newBilleach = (bill + tipValue) / split;
+    let tipEach = tipValue / split;
+
+    //output
+
+    document.getElementById("tipOutput").innerHTML = tipPercent + "%";
+    document.getElementById("splitOutput").innerHTML = split;
+    document.getElementById("newBill").innerHTML = "#" + newBilleach.toFixed(2);
+    document.getElementById("tipEach").innerHTML = "#" + tipEach.toFixed(2);
+}
